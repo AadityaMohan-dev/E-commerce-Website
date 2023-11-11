f9b7a35f50d0f9718eff75145e4392499805c9c1
Aaditya mohan <aadimohan51@gmail.com>
2023-11-12 00:06:02 +0530

    delete old data

--- e-commerce-backend/src/main/java/com/ecommerce/website/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.website.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.website.entity.User;
-
-public interface UserDao extends JpaRepository<User, String>{
-
-}
