f946009b10fa61c11a882fccfdc5de2eb9ae2505
Aaditya mohan <aadimohan51@gmail.com>
2023-11-23 11:15:08 +0530

    model corrections

--- e-commerce.web/src/main/java/com/emommerce/web/models/Product.java
+++ e-commerce.web/src/main/java/com/emommerce/web/models/Product.java
@@ -17,6 +17,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String productName;
+    private String productCompany;
     private String productPrice;
     
 }
